f06a6578eae45cc6e718f0b78a4aecfedc32e445
Jake <jake@jakebladt.com>
2016-02-12 17:20:17 -0500

    router.get->server.get

--- node02/index.js
+++ node02/index.js
@@ -4,13 +4,16 @@ var path = require('path');
 var router = express.Router();
 
 var server = express();
+
 server.set('views', path.join(__dirname, 'views'));
 server.set('images', path.join(__dirname, 'images'));
 
-server.listen(8899, function() {
-	console.log("Server listening on port 8899");
-})
-
-router.get('/', function(req, res) {
+server.get('/', function(req, res) {
 	res.send('You found the front page, skipper!');
 });
+
+server.listen(8888, function() {
+	console.log("Server listening on port 8888");
+})
+
+module.exports = router;
