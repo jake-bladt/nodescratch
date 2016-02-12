2f2b9f0e1a83ebf8f250992a44e5f75c85223be4
Jake <jake@jakebladt.com>
2016-02-12 16:57:29 -0500

    route to /

--- node02/index.js
+++ node02/index.js
@@ -1,6 +1,14 @@
 var express = require('express');
+var router = express.Router();
 
 var server = express();
+server.set('views', path.join(__dirname, 'views'));
+server.set('images', path.join(__dirname, 'images'));
+
 server.listen(8899, function() {
 	console.log("Server listening on port 8899");
 })
+
+router.get('/', function(req, res) {
+	res.send('You found the front page, skipper!');
+});
