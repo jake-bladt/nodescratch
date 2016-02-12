15a53fe79f06ddc7df0585305f9596fb1d47cb4a
Jake <jake@jakebladt.com>
2016-02-12 15:32:37 -0500

    +app file

--- node01/node01/app.js
+++ node01/node01/app.js
@@ -1,1 +1,7 @@
-﻿
+﻿var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'context-type': 'text/plain' });
+    res.end('=^..^=\n');
+}).listen(8888, '127.0.0.1');
+
+console.log('Server running on port 8888');
