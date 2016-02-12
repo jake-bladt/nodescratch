fe04e73e0330087a8b9423656e9801c847ea6e95
Jake <jake@jakebladt.com>
2016-02-12 18:40:59 -0500

    +whisker

--- node02/index.js
+++ node02/index.js
@@ -2,15 +2,12 @@ var express = require('express');
 var path = require('path');
 
 var router = express.Router();
-
 var server = express();
 
 server.set('view engine', 'ejs');
 
 server.set('views', path.join(__dirname, 'views'));
 server.set('images', path.join(__dirname, 'images'));
-
-server.use(require('./home'));
 
 server.get('/secret', function(req, res) {
 	res.status(404).send('There are no secrets here.');
